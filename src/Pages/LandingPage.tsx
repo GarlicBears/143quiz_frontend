49dc5337c14e46ab57bc1781489be24d0d7e9612
jeon602 <731aa@naver.com>
2024-06-04 16:22:17 +0900

    LandingPage.tsx edited

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const LandingPage = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/hello');
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
-    <>
-      <div>Landing Page</div>
-    </>
+    <div>
+      <h1>Landing Page</h1>
+      <div>{data}</div>
+    </div>
   );
 };
 export default LandingPage;
