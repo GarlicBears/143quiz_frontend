9ee87712ec0699c5c96f8c9d4aef6bb3ff5ffcc1
HyunJin, Lee <nagi5924@gmail.com>
2024-06-05 13:09:59 +0900

    test : 로컬 구동 테스트

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const LandingPage = () => {
+  const [msg, setMsg] = useState('' as string);
+  const test = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/hello');
+      console.log(response.data);
+      setMsg(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    test();
+  }, []);
   return (
     <>
-      <div>Landing Page</div>
+      <h1>Landing Page</h1>
+      <h2>{msg}</h2>
     </>
   );
 };
