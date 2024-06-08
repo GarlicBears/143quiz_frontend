6a689c7f8894be6211c6ec2c787eb50da20ec873
HyunJin, Lee <nagi5924@gmail.com>
2024-06-08 20:08:18 +0900

    [GB-2] design : LoadingSpinner 추가

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
-import CustomButton from '../Components/Common/CustomButton';
-import ModalButton from '../Components/Common/ModalButton';
 
 const LandingPage = () => {
-  return (
-    <>
-      <Flex justifyContent="center" alignItems="center" flexDirection="column">
-        <CustomButton text="게임 시작" variant="solid" />
-        <ModalButton
-          type="default"
-          buttonText="Modal button"
-          variant="outline"
-          img="https://via.placeholder.com/150"
-          imgAlt="placeholder"
-          contentText="모달 내용"
-        />
-      </Flex>
-    </>
-  );
+  return <></>;
 };
 export default LandingPage;
