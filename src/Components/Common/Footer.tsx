68d273d55db10daac8ac9f737b3dbdc0a70b409d
HyunJin, Lee <nagi5924@gmail.com>
2024-06-10 12:29:58 +0900

    [GB-10] design : Footer 컴포넌트 분리

---
+++ src/Components/Common/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <>
+      <Box
+        as="footer"
+        width="100%"
+        height="50px"
+        padding="4"
+        bg="var(--bg-color)"
+        color="var(--text-color)"
+        textAlign="center"
+        position="fixed"
+        bottom="0"
+        zIndex="1000"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        boxShadow="md"
+      >
+        &copy; 2024 Garlic Bears
+      </Box>
+    </>
+  );
+};
+export default Footer;
