bb90b78f52e543a9b5ff6e52bdd890f4bd17ceb6
yeon <731aa@naver.com>
2024-06-11 19:47:53 +0900

    UserSignup.tsx : 이메일 직접 입력하기 버튼 수정, '직접 입력'하기 - 다른 도메인 재선택 문제 해결

--- src/Pages/User/UserAgreement.tsx
+++ src/Pages/User/UserAgreement.tsx
@@ -2,17 +2,31 @@ import React, { ChangeEvent, useState } from 'react';
 import {
   Box,
   Button,
-  Checkbox,
   Flex,
-  Heading,
-  Stack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
   Text,
+  useDisclosure,
   VStack,
 } from '@chakra-ui/react';
 
 // 회원 이용 약관 내용
 function UserAgreement() {
   const [isChecked, setIsChecked] = useState(false);
+  const {
+    isOpen: isTermsOpen,
+    onOpen: onTermsOpen,
+    onClose: onTermsClose,
+  } = useDisclosure();
+  const {
+    isOpen: isPrivacyOpen,
+    onOpen: onPrivacyOpen,
+    onClose: onPrivacyClose,
+  } = useDisclosure();
+
   const handleCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
     setIsChecked(e.target.checked);
   };
@@ -20,23 +34,53 @@ function UserAgreement() {
   const handleAllAgree = () => {
     setIsChecked(true);
   };
-  // const handleCheckboxChange = (e) => setIsChecked(e.target.checked);
+
   return (
-    <Box p={5} borderRadius="lg">
+    <Box p={5} borderRadius="lg" borderWidth="1px">
+      <Text fontWeight="bold">서비스 이용약관</Text>
+      <br />
+      <VStack align="stretch" spacing={4}>
+        <Text onClick={onTermsOpen} cursor="pointer" textDecoration="underline">
+          서비스 이용약관 동의(필수)
+        </Text>
+        <Text
+          onClick={onPrivacyOpen}
+          cursor="pointer"
+          textDecoration="underline"
+        >
+          개인정보 수집 및 이용 동의 (필수)
+        </Text>
+      </VStack>
+      <Modal isOpen={isTermsOpen} onClose={onTermsClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box mt={35} p={5} border="1px solid black">
+              <Text>서비스 이용약관 내용</Text>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={isPrivacyOpen} onClose={onPrivacyClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box mt={35} p={5} border="1px solid black">
+              <Text>개인정보처리 방침</Text>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <br />
       <Flex justifyContent="space-between">
-        <Text>서비스 이용약관</Text>
-        <Button justifyContent="flex-end" onClick={handleAllAgree}>
+        <Text>약관 동의 후 회원가입이 가능합니다.</Text>
+        <Button variant="outline" onClick={handleAllAgree}>
           전체동의
         </Button>
       </Flex>
-      <VStack align="stretch" spacing={4}>
-        <Checkbox isChecked={isChecked} onChange={handleCheckboxChange}>
-          서비스 이용약관 동의(필수)
-        </Checkbox>
-        <Checkbox isChecked={isChecked} onChange={handleCheckboxChange}>
-          개인정보 수집 및 이용 동의 (필수)
-        </Checkbox>
-      </VStack>
     </Box>
   );
 }
