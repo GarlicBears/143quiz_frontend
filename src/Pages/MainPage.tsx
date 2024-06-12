0288ab4ba47228da099ef8811ef8c4bb428f81eb
jeon602 <731aa@naver.com>
2024-06-12 20:52:07 +0900

    MainPage.tsx :'이전', '다음'버튼으로 게임 설명 이미지 보기 추가

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
+  Button,
   Card,
   CardBody,
   CardFooter,
   CardHeader,
   Center,
   Heading,
+  Img,
   Text,
 } from '@chakra-ui/react';
 import CustomButton from '../Components/Common/CustomButton';
 
+const photos = [
+  'https://via.placeholder.com/600x400?text=Photo+1',
+  'https://via.placeholder.com/600x400?text=Photo+2',
+  'https://via.placeholder.com/600x400?text=Photo+3',
+  'https://via.placeholder.com/600x400?text=Photo+4',
+];
+
 function MainPage() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handlePreClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? photos.length - 1 : prevIndex - 1,
+    );
+  };
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === photos.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
   return (
     <>
       <Center>
@@ -24,8 +44,32 @@ function MainPage() {
               주제를 고른 후, <br />
               주어진 자음에 맞는 단어를 맟줘보세요
             </Text>
-            <Box mt={5} border="1px solid black" w="100%" h="300px">
-              게임 간략 소개 gif
+            <Box
+              display="flex"
+              mt={5}
+              border="1px solid black"
+              w="100%"
+              h="200px"
+              // alignItems="center"
+            >
+              <Button
+                alignSelf="center"
+                border="1px solid black"
+                onClick={handlePreClick}
+              >
+                이전
+              </Button>
+              <Img
+                src={photos[currentIndex]}
+                alt={`Photo ${currentIndex + 1}`}
+              />
+              <Button
+                alignSelf="center"
+                border="1px solid black"
+                onClick={handleNextClick}
+              >
+                다음
+              </Button>
             </Box>
           </CardBody>
           <CardFooter justifyContent="center">
