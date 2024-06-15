4b457589fab249e6256a4d1d20efbd8dd0d4af32
HyunJin, Lee <nagi5924@gmail.com>
2024-06-15 16:55:21 +0900

    [GB-10] design: Earn Badge(뱃지 획득) 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Game from './Pages/Game';
 import GameLayout from './Components/Game/GameLayout';
 import { RecoilRoot } from 'recoil';
 import GameComplete from './Pages/GameComplete';
+import EarnBadge from './Pages/EarnBadge';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route index element={<Game />} />
           </Route>
           <Route path="/game/complete" element={<GameComplete />} />
+          <Route path="/game/earnbadge" element={<EarnBadge />} />
         </Routes>
       </Router>
     </RecoilRoot>
