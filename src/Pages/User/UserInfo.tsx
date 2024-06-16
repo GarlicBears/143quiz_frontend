67c95728ae15a76314f0e9aecedd1e4a2c99969a
yeon <731aa@naver.com>
2024-06-16 17:23:55 +0900

    유저 정보 조회 기능 추가 : 성별,닉네임, 이미지, 거주지

--- src/Pages/User/UserInfo.tsx
+++ src/Pages/User/UserInfo.tsx
@@ -1,63 +1,102 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Card, Center, Grid, GridItem, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Card,
+  Center,
+  Grid,
+  GridItem,
+  Text,
+  Image,
+} from '@chakra-ui/react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function UserInfo() {
+  const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState({
-    profileImage: '',
     nickname: '',
     gender: '',
+    location: '',
+    image: '',
   });
   useEffect(() => {
     axios
       .get('/api/user/')
       .then((response) => {
-        setUserInfo(response.data);
+        const { data } = response;
+        if (data.success && data.user) {
+          setUserInfo({
+            ...userInfo,
+            nickname: data.user.nickname,
+            gender: data.user.gender,
+            location: data.user.location,
+          });
+        }
       })
       .catch((error) => {
-        console.error('사용자 정보를 불러오는데 실패했습니다.', error);
+        console.error('Failed to load user information', error);
       });
   }, []);
+
+  //회원 정보를 수정할 수 있는 화면으로 이동
+  function handleChangeUserInfo() {
+    navigate('/');
+  }
+
   return (
     <>
       <Center>
-        <Card w="100%" gap={2}>
-          <Grid
-            templateAreas={`"header header"
+        <Card border="1px solid black" w="100%" gap={2}>
+          <Box border="1px solid black" onClick={handleChangeUserInfo}>
+            <Grid
+              templateAreas={`"header header"
                   "nav main"
                   "nav footer"`}
-            gridTemplateRows={'50px 1fr 50px'}
-            gridTemplateColumns={'150px 1fr'}
-            h="200px"
-            gap="1"
-            color="blackAlpha.700"
-            fontWeight="bold"
-          >
-            <GridItem
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              pl="2"
-              bg="pink.300"
-              area={'nav'}
+              gridTemplateRows={'50px 1fr 50px'}
+              gridTemplateColumns={'150px 1fr'}
+              h="200px"
+              gap="1"
+              color="blackAlpha.700"
+              fontWeight="bold"
             >
-              <Box border="1px solid black">전희연</Box>{' '}
-            </GridItem>
-            <GridItem pl="2" bg="green.300" area={'main'}>
-              유저의 닉네임
-            </GridItem>
-            <GridItem pl="2" bg="blue.300" area={'footer'}>
-              유저 성별 정보
-            </GridItem>
-          </Grid>
-          {/*</Box>*/}
+              <GridItem
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                pl="2"
+                border="1px solid black"
+                area={'nav'}
+              >
+                <Box border="1px solid black" borderRadius="full">
+                  {userInfo.image ? (
+                    <Image
+                      src={userInfo.image}
+                      borderRadius="full"
+                      boxSize="50px"
+                      alt="Profile Image"
+                    />
+                  ) : (
+                    'No image' //이미지가 없을 경우 no image로 표시
+                  )}
+                </Box>
+              </GridItem>
+              <GridItem pl="2" border="1px solid black" area={'main'}>
+                Nickname: {userInfo.nickname}
+              </GridItem>
+              <GridItem pl="2" border="1px solid black" area={'footer'}>
+                Gender: {userInfo.gender}
+                <br />
+                Location: {userInfo.location}
+              </GridItem>
+            </Grid>
+          </Box>
           <Box
             mt={5}
             fontSize="xl"
             justifyContent="center"
             border="1px solid red"
           >
-            내가 모은 뱃지{' '}
+            내가 모은 뱃지
           </Box>
           <Text mt={5} fontSize="xl">
             게임 설정
