fadf3bad4a69fa4eb8161ef751022c5b99df60ab
yeon <731aa@naver.com>
2024-06-16 19:42:21 +0900

    유저 정보 수정 화면 파일 기능 추가 중.

--- src/Pages/User/UserInfoUpdate.tsx
+++ src/Pages/User/UserInfoUpdate.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 
 function UserInfoUpdate() {
-  return <div></div>;
+  return (
+    <Center>
+      <VStack
+        border="1px solid black"
+        w="100%"
+        p={2}
+        spacing={3}
+        align="stretch"
+      >
+        <Box textAlign="center">
+          <Image
+            // src={userImage}
+            borderRadius="full"
+            boxSize="150px"
+            margin="auto"
+          />
+          <Input
+            type="file"
+            accept="image/*"
+            position="absolute"
+            left="50%"
+            top="50%"
+            transform="translate(-50%, -50%)"
+            opacity={0}
+            aria-label="Update profile image"
+            size="lg"
+          />
+          <Button variant="ghost" textDecoration="underline" mt={4}>
+            이미지 업로드
+          </Button>
+          <Text>내가 모은 뱃지로 프로필 설정하기</Text>
+        </Box>
+        <Divider />
+        <Text>이용 동의 및 개인정보 처리방침</Text>
+        <Text>로그아웃</Text>
+        <Text>회원탈퇴</Text>
+      </VStack>
+    </Center>
+  );
 }
 
 export default UserInfoUpdate;
