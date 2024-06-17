c84f86a461a0f2ac644f3e31ebce6fee203d6865
yeon <731aa@naver.com>
2024-06-17 10:23:35 +0900

    계정 삭제, 계정 로그아웃

---
+++ src/Pages/User/UserAccountDelete.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserAccountDelete() {
+  return <div></div>;
+}
+
+export default UserAccountDelete;
---
+++ src/Pages/User/UserLogout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserLogout() {
+  return <div></div>;
+}
+
+export default UserLogout;
