6837676c1029b207519534ebd759b4b1c33caa3f
전희연 <143367041+jeon602@users.noreply.github.com>
2024-07-04 21:07:10 +0900

    Gb 94 logout0702 (#23)
    * [fix]- 회원가입 화면 이메일 중복 체크 시 @ 도메인 주소 뜨도록 수정
    
    * [fix]- 회원가입 화면 이메일 중복 체크 시 @ 도메인 주소 뜨도록 수정
    
    * [fix]- 이전 커밋 수정 : axios 를 'axiosInstance'로 변경함
    
    * [feat]- UserLogout.tsx 로그아웃 시 쿠키 리프레쉬 토큰으로 로그아웃 요청 (추가 중)
    
    * [fix]- UserInfo :유저 정보 조회 수정
    UserLogout : 쿠키 RT 로그아웃 요청, 로그아웃 시 랜딩페이지로 이동
    
    * [remove]- 로그아웃 화면 : 주석 제거 / 로그아웃 후 다시 로그인, 토큰 발급 확인
    
    * [feat] - axiosInstance : refreshToken 으로 accessToken 재발급
    
    * [fix] - axiosInstance.ts : accessToken 보내지 않을 url 목록 추가
    
    * fix: /user/reissue 요청할 때 accessToken 제거
    
    * userinfo 사용자 정보 조회
    
    ---------
    
    Co-authored-by: Jeong Jihwan <wlghks0461@gmail.com>

--- src/pages/user/UserLogout.tsx
+++ src/pages/user/UserLogout.tsx
@@ -10,10 +10,45 @@ import {
   ModalOverlay,
   Text,
   useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
+import axiosInstance from '../../api/axiosInstance';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 function UserLogout() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    const refreshToken = Cookies.get('refreshToken');
+    axiosInstance
+      .delete('/user/logout', {
+        headers: { Authorization: `Bearer ${refreshToken}` },
+      })
+      .then((response) => {
+        Cookies.remove('accessToken');
+        Cookies.remove('refreshToken');
+        toast({
+          description: '성공적으로 로그아웃 되었습니다',
+          status: 'success',
+        });
+        navigate('/');
+      })
+      .catch((error) => {
+        console.error('로그아웃에 실패했습니다.', error);
+        toast({
+          description: '로그아웃 도중 에러가 발생했습니다',
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        onClose();
+      });
+  };
+
   return (
     <>
       <Flex onClick={onOpen}>
@@ -33,10 +68,7 @@ function UserLogout() {
             <Button flex="1" variant="outline" mr={3} onClick={onClose}>
               취소
             </Button>
-            <Button
-              flex="1"
-              // onClick={handleLogout} //[TODO] 로그아웃 로직 추가
-            >
+            <Button flex="1" onClick={handleLogout}>
               로그아웃
             </Button>
           </ModalFooter>
