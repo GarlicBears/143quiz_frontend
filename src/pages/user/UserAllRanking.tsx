d6a005ed0b6ae023a77d3450fd2002d242280936
yeon <731aa@naver.com>
2024-07-08 03:21:33 +0900

    [feat]-UserAllRanking: 전체 게임 랭킹 api 연동 성공 TODO: 데이터 보이도록 수정

--- src/pages/user/UserAllRanking.tsx
+++ src/pages/user/UserAllRanking.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Accordion,
   AccordionButton,
@@ -8,21 +8,49 @@ import {
   Box,
   Card,
   Center,
-  Divider,
   Heading,
+  List,
+  ListItem,
+  Spinner,
 } from '@chakra-ui/react';
+import axiosInstance from '../../api/axiosInstance';
+
+interface Ranking {
+  // userId: number;
+  nickname: string;
+  totalBadges: number;
+  totalHearts: number;
+  rank: number;
+}
+
 // 전체 뱃지 수 하트 수를 기준으로 랭킹 정보 조회
 // 토글로 뱃지 랭킹 주제 랭킹 조회
 const UserAllRanking = () => {
-  useEffect(() => {});
+  const [rankingData, setRankingData] = useState<Ranking[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axiosInstance
+      .get('/game/rankings')
+      .then((response) => {
+        if (response.data.success) {
+          setRankingData(response.data.ranking);
+        }
+      })
+      .catch((error) => {
+        console.error('랭킹 데이터 조회 중 오류 발생', error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
       <Center>
         <Card w="100%">
           <Heading textAlign="center" mt={20}>
-            {' '}
-            Game Ranking{' '}
+            Game Ranking
           </Heading>
           <Box border="2px solid" m={20}>
             <Accordion allowMultiple>
@@ -35,7 +63,21 @@ const UserAllRanking = () => {
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>abcd12341234</AccordionPanel>
+                {/*<AccordionPanel pb={4}>abcd12341234</AccordionPanel>*/}
+                <AccordionPanel pb={4}>
+                  {loading ? (
+                    <Spinner />
+                  ) : (
+                    <List spacing={3}>
+                      {rankingData.map((user) => (
+                        <ListItem key={user.nickname}>
+                          {user.nickname} - Badges: {user.totalBadges}, Hearts:{' '}
+                          {user.totalHearts}
+                        </ListItem>
+                      ))}
+                    </List>
+                  )}
+                </AccordionPanel>
               </AccordionItem>
 
               <AccordionItem mt={20}>
